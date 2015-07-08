e6320de5373dbda86affebdf15c0b8100e1a2363
Daniel Carlos Hovadick Félix <danielcarloshf@hotmail.com>
2015-07-08 12:25:28 -0300

    Solved clone and copy issues

--- src/main.rb
+++ src/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 $LOAD_PATH << '.'
 
+require 'io/console'
 require 'clone.rb'
 require 'files.rb'
 require 'search.rb'
@@ -12,16 +13,27 @@ include Search
 destination = ARGV[0]
 language = 'javascript'
 
+print "GitHub username: "
+username = STDIN.gets.chomp
+print "Enter your password: "
+password = STDIN.noecho(&:gets).chomp 
+puts
+
 puts "Searching repositories..."
-Search.find_repositories(language).each_pair do |repo, clone_url|
-    path = destination + File::SEPARATOR + repo
-    puts "Cloning \"#{repo}\"..."
-    Clone.git_clone(clone_url, path)
-    puts "Selecting only first-party files..."
-    files = Files.only_first_party(path, language)
-    puts "Copying selected files..."
-    Files.copy_with_path(files, path, path + '_src')
-    puts "Removing \"#{path}\"..."
-    Files.remove_cloned_repo(path)
+Search.find_repositories(username, password, language).each_pair do |repo, clone_url|
+    begin
+        path = destination + File::SEPARATOR + repo
+        puts "Cloning \"#{repo}\"..."
+        Clone.git_clone(clone_url, path)
+        puts "Selecting only first-party files..."
+        files = Files.only_first_party(path, language)
+        puts "Copying selected files..."
+        Files.copy_with_path(files, path, path + '_src')
+        puts "Removing \"#{path}\"..."
+        Files.remove_cloned_repo(path)
+    rescue Exception => msg
+        puts msg
+        puts "On #{repo}: #{clone_url}"
+    end
 end
 puts "DONE"
--- src/search.rb
+++ src/search.rb
@@ -6,43 +6,47 @@ require 'curb'
 module Search
     
     # Find repositories on GitHub for a given language. Results are sorted by the number of stars.
-    def Search.find_repositories(lang)
+    def Search.find_repositories(username, password, lang)
         
         repositories = Hash.new
         
         url = 'https://api.github.com/search/repositories?q=fork:false+language:'+ lang +'+stars:>=1000'
-            
-        response = Curl::Easy.http_get(url) do |curl|
-            curl.headers['User-Agent'] = 'aserg.labsoft.dcc.ufmg.br'
-            curl.headers['Accept'] = 'application/json'
-            curl.headers['Content-Type'] = 'application/json'
-            curl.headers['Api-Version'] = '2.2'
-            curl.ssl_verify_peer = false
-        end
+                    
+        request = Curl::Easy.new(url)
+        request.http_auth_types = :basic
+        request.username = username
+        request.password = password
+        request.ssl_verify_peer = false
+        request.headers['User-Agent'] = 'aserg.labsoft.dcc.ufmg.br'
+        request.headers['Accept'] = 'application/json'
+        request.headers['Content-Type'] = 'application/json'
+        request.headers['Api-Version'] = '2.2'
         
-        result = JSON.parse(response.body)
+        request.perform
+        result = JSON.parse(request.body_str)
         total = result['total_count'].to_i
         puts "#{total} repositories found."
-        num_pages = (total/10.0).ceil
+        num_pages = (total/100.0).ceil
         
         (1..num_pages).each do |page|
-            url = 'https://api.github.com/search/repositories?q=fork:false+language:'+ lang \
-                + '+stars:>=1000&sort=stars&order=desc&page='+ page.to_s +'&per_page=10'
-                
-            response = Curl::Easy.http_get(url) do |curl|
-                curl.headers['User-Agent'] = 'aserg.labsoft.dcc.ufmg.br'
-                curl.headers['Accept'] = 'application/json'
-                curl.headers['Content-Type'] = 'application/json'
-                curl.headers['Api-Version'] = '2.2'
-                curl.ssl_verify_peer = false
-            end
+            url = "https://api.github.com/search/repositories?q=fork:false+language:"+ lang \
+                + "+stars:>=1000&sort=stars&order=desc&page=" + page.to_s + "&per_page=100"
             
-            result = JSON.parse(response.body)
-            result['items'].each do |r|
-                repositories[r['name']] = r['clone_url']
+            puts url
+            
+            request.url = url
+            request.perform
+            result = JSON.parse(request.body_str)
+            
+            begin
+                result['items'].each do |r|
+                    repositories[r['name']] = r['clone_url']
+                end
+            rescue Exception
             end
         end
         
+        puts "Got #{repositories.length} repositories clone url's."
         repositories
     end
 
